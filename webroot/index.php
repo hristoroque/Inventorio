25d8beb2354c20d8a49efecbbea5f7687fdb7b60
hristoroque <aroquega@unsa.edu.pe>
2019-09-29 17:47:59 -0500

    Added something to show

--- webroot/index.php
+++ webroot/index.php
@@ -38,3 +38,6 @@ $server = new Server(new Application(dirname(__DIR__) . '/config'));
 
 // Run the request/response through the application and emit the response.
 $server->emit($server->run());
+?>
+
+<h1> It's working baby </h1>
