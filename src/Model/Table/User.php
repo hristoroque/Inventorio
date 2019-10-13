b64e9c3dba480c5ec4a2acb5c1640c46759b35da
hristoroque <aroquega@unsa.edu.pe>
2019-10-13 15:45:59 -0500

    vanilla cake

---
+++ src/Model/Table/User.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Model\Table;
+use Cake\ORM\Table;
+
+class User extends Table
+{
+	public function initialize(array $config)
+	{
+		
+	}
+}	
+
+
---
+++ src/Template/main.ctp
@@ -0,0 +1,1 @@
+<h1>Hello Fucking world</h1>
