2dbe03ddccf19a5d2c1db7118af1ec3d4c3f1b9b
Contre97 <iamcontrerasalcazar@gmail.com>
2019-11-11 15:36:28 -0500

    generando kardex en base a la venta/compra

--- src/Controller/OperationsCabController.php
+++ src/Controller/OperationsCabController.php
@@ -4,11 +4,14 @@ namespace App\Controller;
 use App\Controller\AppController;
 use App\Model\Entity\OperationsCab;
 use App\Model\Entity\OperationsDet;
+use App\Model\Entity\KardexesDet;
+use Cake\Datasource\ConnectionManager;
 
 /**
  * OperationsCab Controller
  *
  * @property \App\Model\Table\OperationsCabTable $OperationsCab 
+ * @property \App\Model\Table\KardexesDetTable $KardexesDet 
  * @method \App\Model\Entity\OperationsCab[]|\Cake\Datasource\ResultSetInterface paginate($object = null, array $settings = [])
  */
 class OperationsCabController extends AppController
@@ -54,7 +57,7 @@ class OperationsCabController extends AppController
         $operationsCab = $this->OperationsCab->newEntity();
         if ($this->request->is('post')) {
             $operationsCab = $this->OperationsCab->patchEntity($operationsCab, $this->request->getData());
-            echo $operationsCab;
+            //echo $operationsCab;
             $ope_cab = new OperationsCab;
             $ope_cab->user_id = $operationsCab->user_id;
             if( $operationsCab->operation_type == 'comprar')
@@ -63,13 +66,45 @@ class OperationsCabController extends AppController
                 $ope_cab->operation_type_id = 2;
             else 
                 $ope_cab->operation_type_id = 1;
-            echo $ope_cab;
+            //echo $ope_cab;
 
             $ope_det = new OperationsDet;            
             $ope_det->article_id = $operationsCab->article_id;
             $ope_det->quantity = $operationsCab->quantity;
-            echo($ope_det);
+            //echo($ope_det);
+            
+            $connection = ConnectionManager::get('default');
+            $results_kardex = $connection->execute('select * from kardexes_cab where article_id = '.$operationsCab->article_id.'')->fetchAll('assoc');
+            $kardex_id = ($results_kardex[0]);
+            print_r($kardex_id);            
+            echo '</br>';
 
+            $results_article = $connection->execute('select * from articles where id = '.$operationsCab->article_id.'')->fetchAll('assoc');
+            print_r($results_article);
+
+            echo '</br>';
+            $kardex_det = new KardexesDet;
+            $kardex_det->kardex_cab_id = $results_kardex[0]['id'];
+            if( $operationsCab->operation_type == 'comprar'){            
+                $kardex_det->entry_amount = $operationsCab->quantity;
+                $kardex_det->entry_unit_price = $results_article[0]['buy_price'];
+                $kardex_det->entry_total_price = $operationsCab->quantity*$results_article[0]['buy_price'];
+                $kardex_det->output_amount = 0;
+                $kardex_det->output_unit_price=0;
+                $kardex_det->output_total_price=0;
+            }
+            if( $operationsCab->operation_type == 'vender'){            
+                $kardex_det->entry_amount  = 0;
+                $kardex_det->entry_unit_price = 0;
+                $kardex_det->entry_total_price = 0;
+                $kardex_det->output_amount = $operationsCab->quantity;
+                $kardex_det->output_unit_price = $results_article[0]['sell_price'];;
+                $kardex_det->output_total_price = $operationsCab->quantity*$results_article[0]['sell_price'];;
+            }
+            $kardex_det->existence_current_stock = $results_kardex[0]['current_stock'];
+            $kardex_det->existence_current_balance = $results_kardex[0]['current_balance'];
+            echo $kardex_det;
+            
             if ($this->OperationsCab->save($ope_cab)) {
                 $this->Flash->success(__('The operations cab has been saved.'));
                 $last_id=$ope_cab->id; 
@@ -77,6 +112,28 @@ class OperationsCabController extends AppController
                 $ope_det->operation_cab_id = $last_id;
                 $this->loadModel('OperationsDet');
                 $this->OperationsDet->save($ope_det);
+
+                $last_created=$ope_cab->created;             
+                $kardex_det->date = $last_created;
+                $this->loadModel('KardexesDet');
+                $this->KardexesDet->save($kardex_det);
+
+            if( $operationsCab->operation_type == 'vender'){
+                $connection->execute(' update kardexes_cab
+                set current_stock = '.($results_kardex[0]['current_stock'] - $kardex_det->output_amount).',
+                    current_balance = '.($results_kardex[0]['current_balance'] + $kardex_det->output_total_price).', 
+                    modified = CURRENT_TIMESTAMP
+                where id = '.$results_kardex[0]['id'].'');
+            }           
+            
+            if( $operationsCab->operation_type == 'comprar'){
+                $connection->execute(' update kardexes_cab
+                set current_stock = '.($results_kardex[0]['current_stock'] + $kardex_det->entry_amount).', 
+                    current_balance = '.($results_kardex[0]['current_balance'] - $kardex_det->entry_total_price).', 
+                    modified = CURRENT_TIMESTAMP
+                where id = '.$results_kardex[0]['id'].'');
+            }       
+
                 return $this->redirect(['action' => 'index']);
             }
             $this->Flash->error(__('The operations cab could not be saved. Please, try again.'));
